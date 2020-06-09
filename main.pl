bed1b3060903ff6f53378d45fe6e85368ee1d545
Sven Wiltink <sven.wiltink@hotmail.com>
2020-06-09 17:05:52 +0200

    implemented a basic command router and added the butts command

--- main.pl
+++ main.pl
@@ -1,6 +1,16 @@
 :- [discord].
 :- use_module(library(http/http_client)).
 
+command_prefix("!prolog").
+baas_endpoint("https://butts.ndumas.com").
+
+bot_token(Token):-
+    getenv("DISCORDPL_TOKEN", Token).
+
+get_butt(Butt):-
+    baas_endpoint(Endpoint),
+    http_get(Endpoint, Butt, []).
+
 custom_handler(Client, "MESSAGE_CREATE", Message):-
     \+ get_dict(bot, Message.author, true),
     format("Custom handler received message create: ~p\n", [Message]),
@@ -8,12 +18,18 @@ custom_handler(Client, "MESSAGE_CREATE", Message):-
 
 handle_command(Client, Message):-
     normalize_space(atom(Command), Message.content),
-    sub_string(Command, 0, _, 0, "!prolog ping"),
-    discord_message_create(Client, Message.channel_id, "PONG", Response),
-    format("sent pong: ~p", Response).
+    command_prefix(Prefix),
+    sub_string(Command, 0, _, _, Prefix),
+    split_string(Command, " ", [], [_|Subcommands]),
+    handle_subcommand(Client, Message, Subcommands).
 
-bot_token(Token):-
-    getenv("DISCORDPL_TOKEN", Token).
+handle_subcommand(Client, Message, ["ping"|[]]):-
+    discord_message_create(Client, Message.channel_id, "PONG", _).
+
+handle_subcommand(Client, Message, ["butt"|[]]):-
+    get_butt(Butt),
+    format(atom(ButtString), "```\n~w\n```", [Butt]),
+    discord_message_create(Client, Message.channel_id, ButtString, _).
 
 :-
     bot_token(Token),
